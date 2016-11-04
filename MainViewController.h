a6f5949e9454887479e96f2249d5d4270f32521a
Berkay <Berkay_Sebat@yahoo.com>
2016-11-04 11:17:33 -0500

    added a new view controller

---
+++ MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Practice
+//
+//  Created by macbook pro on 11/4/16.
+//  Copyright © 2016 berkaysebat.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
